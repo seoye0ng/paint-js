83c5e29dd9f475397fbdfee47639095d53d663ef
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-06-20 19:26:36 +0900

    2.5 Filling Mode

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-canvas.width = 800;
-canvas.height = 800;
+const INITIAL_COLOR = "#2c2c2c"; //이런 variable들은 반복하게되면 만든다.
+const CANVAS_SIZE = 800;
 
-ctx.strokeStyle = "#2c2c2c"; //첫 번째색(검정색)으로 시작하도록 설정
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR; //첫 번째색(검정색)으로 시작하도록 설정
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -36,6 +40,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color; //color를 클릭하면 strokeStyle과 fillStyle을 color값으로 설정
 }
 
 function handleRangeChange(event) {
@@ -53,11 +58,18 @@ function handleModeClick() {
   }
 }
 
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE); //=ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPaninting);
   canvas.addEventListener("mouseleave", stopPaninting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
